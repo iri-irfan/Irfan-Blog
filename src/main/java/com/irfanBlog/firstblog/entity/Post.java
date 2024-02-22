da0f9c307ebd6a318be82022dfa615743df527bf
Md <iri.irfan007@gmail.com>
2024-02-22 13:40:07 +0530

    111: Post Class Added in Entity

---
+++ src/main/java/com/irfanBlog/firstblog/entity/Post.java
@@ -0,0 +1,23 @@
+package com.irfanBlog.firstblog.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name = "posts")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private long id;
+    private String title;
+    private String description;
+    private String content;
+
+}
---
+++ src/test/java/com/irfanBlog/firstblog/FirstblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irfanBlog.firstblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
